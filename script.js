000b35e26b1ae1a829f4fd9d0b5b5f539e54c279
Kevin <kphyde@gmail.com>
2018-10-06 15:15:05 -0700

    delete computer block html, save for later game updates, change computer and cpu references in index.html and script.js to reference player2 instead (in various applications)

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // HTML WRITING VARIABLES
 var userText = document.getElementById("user");
-var computerText = document.getElementById("computer");
+var playerTwoText = document.getElementById("playerTwo");
 var result = document.getElementById("result");
 var userScore = document.getElementById("userScore");
-var computerScore = document.getElementById("computerScore");
+var playerTwoScore = document.getElementById("playerTwoScore");
 var html_user_image = document.getElementById("user_image");
-var html_cpu_image = document.getElementById("cpu_image");
+var html_p2_image = document.getElementById("p2_image");
 var num_games = document.getElementById("games");
 
 // GAME READY DOM
 function gameChoiceReset() {
   html_user_image.src = "images/placeholder.jpg"
-  html_cpu_image.src = "images/placeholder.jpg"
+  html_p2_image.src = "images/placeholder.jpg"
 }
 
 // GAME DOM UPDATES
@@ -39,7 +39,7 @@ function resultTextUpdate(winLose) {
 
 function scoreUpdates() {
   userScore.textContent = userScore_count;
-  computerScore.textContent = computerScore_count;
+  playerTwoScore.textContent = playerTwoScore_count;
 }
 
 // SET THE FIREBASE VARIABLES
@@ -56,10 +56,43 @@ fbRef.set({
   ties: 0,
 })
 
+// OPEN A LOG IN MODAL
+$(window).on('load', function () {
+  // can we wrap this in an 'if' to check local storage for a username?
+  $('#usernameModal').modal('show');
+});
+
+// save username to local var and to database // !!!! UPDATE TO MY PARAMS !!!!!
+$(document).on("click", ".username-btn", function () {
+  username = $("#username").val().trim();
+  var fNameRef = database.ref('users/' + username);
+  fNameRef.transaction(function (currentData) {
+    if (currentData === null) {
+      return { fChoice: "" };
+    } else {
+      console.log('User already exists.');
+      $('#usernameModalTitle').text('That name is already in use.')
+      return; // Abort the transaction.
+    }
+  }, function (error, committed) {
+    if (error) {
+      console.log('Transaction failed abnormally!', error);
+    } else if (!committed) {
+      console.log('We aborted the transaction (because ' + username + ' already exists).');
+    } else {
+      console.log('User ' + username + ' added!');
+      // check and create roomnumber
+      findRoom();
+      database.ref('users/' + username).onDisconnect().remove();
+      $('#usernameModal').modal('hide');
+    }
+  });
+});
+
 // SET THE USER AND COMPUTER VARIABLES      
 var computerChoices = ["rock", "paper", "scissors"];
 var userScore_count = 0;
-var computerScore_count = 0;
+var playerTwoScore_count = 0;
 var num_games_count = 0;
 var userGuess;
 var computerGuess = function () { return computerChoices[Math.floor(Math.random() * computerChoices.length)]; }
@@ -68,15 +101,15 @@ var computerReturn = function (play) {
   switch (play) {
     case "rock":
       text = "ROCK"
-      html_cpu_image.src = "images/rock.jpg"
+      html_p2_image.src = "images/rock.jpg"
       break;
     case "paper":
       text = "PAPER"
-      html_cpu_image.src = "images/paper.jpg"
+      html_p2_image.src = "images/paper.jpg"
       break;
     case "scissors":
       text = "SCISSORS"
-      html_cpu_image.src = "images/scissors.jpg"
+      html_p2_image.src = "images/scissors.jpg"
       break;
   }
   return text;
@@ -110,7 +143,7 @@ let game = function (user, computer) {
       userScore_count = userScore_count + 1;
       resultTextUpdate('win');
     } else if (computer == "paper") {
-      computerScore_count = computerScore_count + 1;
+      playerTwoScore_count = playerTwoScore_count + 1;
       resultTextUpdate('lose');
     }
   } else if (user == "paper") {
@@ -118,7 +151,7 @@ let game = function (user, computer) {
       userScore_count = userScore_count + 1;
       resultTextUpdate('win');
     } else if (computer == "scissors") {
-      computerScore_count = computerScore_count + 1;
+      playerTwoScore_count = playerTwoScore_count + 1;
       resultTextUpdate('lose');
     }
   } else if (user == "scissors") {
@@ -127,7 +160,7 @@ let game = function (user, computer) {
       resultTextUpdate('win');
 
     } else if (computer == "rock") {
-      computerScore_count = computerScore_count + 1;
+      playerTwoScore_count = playerTwoScore_count + 1;
       resultTextUpdate('lose');
     }
   }
@@ -146,3 +179,15 @@ let game = function (user, computer) {
 // DOM READS FBDB OBJECT TO SELECT AND WRITE WIN/LOSE MESSAGE ON USERS CLIENT, UPDATE GAME, W/L COUNTS
 // RESET BUTTON TO SET DOM TO ALLOW PICKS FOR NEXT GAME AND REPEAT
 
+
+// PLAYER 2 FOR WHEN THE GAME STARTS
+{/* <div class="col-md-6">
+        <div class="card score_card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">COMPUTER:</h5><img alt="rock paper scissors image" class="game_image" id="p2_image"
+              src="images/placeholder.jpg">
+            <h1 class="text_result" id="computer"></h1><span class="text_result">score:</span> <span class="score_keep"
+              id="playerTwoScore"></span>
+          </div>
+        </div>
+      </div> */}
